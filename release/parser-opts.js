8b002591a3813d0631e556e54b02b00427c68ff2
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-05-27 23:34:09 +0200

    feat(release): use conventionalcommits

--- release/parser-opts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  headerCorrespondence: [`type`, `scope`, `subject`],
-  headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-  noteKeywords: [`BREAKING CHANGE`],
-  revertCorrespondence: [`header`, `hash`],
-  revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
-};
