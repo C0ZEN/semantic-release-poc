b720491c8910fe131a674a25c0ff93142035cba3
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-05-27 22:34:09 +0200

    feat(release): try to use js config

--- release.config-old.js
+++ release.config-old.js
@@ -86,95 +86,18 @@ customTransform = (commit, context) => {
 };
 
 module.exports = {
-  branches: [
-    "master"
-  ],
+  releaseRules,
+  parserOpts,
+  writerOpts: { transform: customTransform },
   plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      "@semantic-release/commit-analyzer",
+      "@semantic-release/exec",
       {
-        preset: "angular",
-        releaseRules: [
-          {
-            "type": "feat",
-            "release": "minor"
-          },
-          {
-            "type": "fix",
-            "release": "patch"
-          },
-          {
-            "type": "perf",
-            "release": "patch"
-          }
-        ],
-        "parserOpts": {
-          "noteKeywords": [
-            "BREAKING CHANGE",
-            "BREAKING CHANGES",
-            "BREAKING"
-          ]
-        }
+        verifyReleaseCmd: "echo ${nextRelease.version} > version.txt"
       }
     ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        "preset": "angular",
-        "parserOpts": {
-          "noteKeywords": [
-            "BREAKING CHANGE",
-            "BREAKING CHANGES",
-            "BREAKING"
-          ]
-        },
-        "writerOpts": {
-          "title": "this_is_the_title",
-          "host": "https://github.com",
-          "commitsSort": [
-            "subject",
-            "scope"
-          ],
-          "owner": "C0ZEN",
-          "repoUrl": "https://github.com/C0ZEN/semantic-release-poc",
-          "linkReferences": true,
-          "commit": "commits",
-          "issue": "issues",
-          "includeDetails": false,
-          "ignoreReverted": false,
-          "transform": "() => 'hello'"
-        },
-        "linkCompare": true,
-        "linkReferences": true,
-        "commit": "commit",
-        "issue": "issues"
-      }
-    ],
-    [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "CHANGELOG.md",
-        "changelogTitle": "# Semantic Versioning Changelog"
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        "assets": [
-          "package.json",
-          "package-lock.json",
-          "CHANGELOG.md"
-        ],
-        "message": "build(release): new version ${nextRelease.version}"
-      }
-    ],
-    [
-      "@semantic-release/github"
-    ]
-  ],
-  "dryRun": false,
-  "ci": true,
-  "preset": "angular",
-  "debug": true,
-  "tagFormat": "${version}"
+    "@semantic-release/github"
+  ]
 };
